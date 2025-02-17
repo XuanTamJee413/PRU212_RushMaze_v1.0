e88f9cb8e3bae548f3ae4619b18ce2d37894997d
Nguyễn Xuân Tâm <xuantamjee413@gmail.com>
2025-02-17 23:16:09 +0700

    health system

--- RushMaze_v1.0/Assets/Scripts/HealthSystem.cs
+++ RushMaze_v1.0/Assets/Scripts/HealthSystem.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HealthSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private float maxHealth = 100f; 
+    private float currentHealth;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth; 
+        UpdateHealthUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    // bi ban trung
+    public void TakeDamage(float damage)
     {
-        
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+    // phuogn thuc hoi mau
+    public void Healing(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthUI();
+    }
+
+    // cap nhat UI cho thanh mau
+    private void UpdateHealthUI()
+    {
+        healthSlider.value = currentHealth / maxHealth;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        Debug.Log("Player died!");
+        gameObject.SetActive(false); // hide Player
     }
 }
