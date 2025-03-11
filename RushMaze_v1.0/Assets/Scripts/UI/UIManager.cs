fc40e525616ae2027ff58ddc3ded05d9f193bd51
Tamnx <xuantamjee413@gmail.com>
2025-03-11 22:25:16 +0700

    UPdate UI #1

---
+++ RushMaze_v1.0/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_Text hpText;
+    public TMP_Text manaText;
+    public TMP_Text goldText;
+    public TMP_Text powerText;
+    public TMP_Text expText;
+    public TMP_Text levelText;
+
+
+    private void Start()
+    {
+        LoadAndDisplay();
+    }
+
+    // Tải và hiển thị dữ liệu nhân vật
+    void LoadAndDisplay()
+    {
+        PlayerData player = SaveSystem.LoadPlayer();
+
+        hpText.text = $"HP: {player.CurrentHp}/{player.MaxHp}";
+        manaText.text = $"Mana: {player.CurrentMana}/{player.MaxMana}";
+        goldText.text = $"Gold: {player.Gold}";
+        powerText.text = $"Power: {player.Power}";
+        expText.text = $"Exp: {player.Experience}";
+        levelText.text = $"Level: {player.Level}";
+    }
+
+    // Cập nhật UI sau khi thay đổi dữ liệu
+    public void UpdateUI(PlayerData player)
+    {
+        hpText.text = $"HP: {player.CurrentHp}/{player.MaxHp}";
+        manaText.text = $"Mana: {player.CurrentMana}/{player.MaxMana}";
+        goldText.text = $"Gold: {player.Gold}";
+        powerText.text = $"Power: {player.Power}";
+        expText.text = $"Exp: {player.Experience}";
+        levelText.text = $"Level: {player.Level}";
+    }
+}
